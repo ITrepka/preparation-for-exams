d6363130584088be9badabc276e9cdb4d7d0a067
Ireneusz Trepka <irek.trepka@gmail.com>
2019-11-21 21:36:52 +0100

    initial commit

---
+++ src/main/java/com/craftincode/exams/basic/Anagram.java
@@ -0,0 +1,9 @@
+package com.craftincode.exams.basic;
+
+//Napisz poniższej metody, która sprawdza czy word1 jest anagramem word2 (anagram jest to słowo utworzone z liter innego
+//słowa, gdzie każda z liter może być wykorzystana tylko raz np.wiatr jest anagramem słowa klawiatura, natomiast wiatraki już nie
+
+
+public class Anagram {
+
+}
